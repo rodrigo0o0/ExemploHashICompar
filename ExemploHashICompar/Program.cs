ab47dcc83d8defd864250ae5fa1c93d22cc688b5
Rodrigo Sandeski <sandeski47@gmail.com>
2022-04-22 12:54:21 -0300

    Finished tests

--- ExemploHashICompar/Program.cs
+++ ExemploHashICompar/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ExemploHashICompar.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,27 @@ namespace ExemploHashICompar
     {
         static void Main(string[] args)
         {
+            HashSet<Product> a = new HashSet<Product>();
+            a.Add(new Product("TV", 900.00));
+            a.Add(new Product("Notebook", 1200.00));
+
+            HashSet<Point> b= new HashSet<Point>();
+            b.Add(new Point(3, 4));
+            b.Add(new Point(5, 10));
+
+            Product prod = new Product("Notebook", 1200.00);
+            foreach (var item in a)
+            {
+                Console.WriteLine(item.GetHashCode());
+            }
+            Console.WriteLine(a.Contains(prod));
+
+            Point point = new Point(3, 4);
+
+            Console.WriteLine(b.Contains(point));
+
+
+            Console.ReadKey();
         }
     }
 }
